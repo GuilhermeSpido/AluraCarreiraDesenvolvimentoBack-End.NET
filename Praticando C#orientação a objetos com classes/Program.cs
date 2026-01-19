5cdf8f6956fa6bbaa9a91fa8ce7f499af4c8099e
GuilhermeSpido <spidoguilherme12@gmail.com>
2026-01-18 22:21:28 -0300

    Curso Praticando C#: orientação a objetos com classes concluído

--- Praticando C#orientação a objetos com classes/Program.cs
+++ Praticando C#orientação a objetos com classes/Program.cs
@@ -1,8 +1,9 @@
-﻿//1 Crie uma classe chamada Passagem que tenha:
+﻿using System.Globalization;
+
+//1 Crie uma classe chamada Passagem que tenha:
 //Uma propriedade pública Passageiro.
 //Uma propriedade pública Destino.
 //Um construtor que obrigue o preenchimento dessas informações na criação do objeto.
-
 
 Passagem passagem = new Passagem("Guilherme Duarte Spido", "Caxias do Sul");
 
@@ -81,6 +82,118 @@ catch
     Console.WriteLine("sefodeu");
 }
 
+
+//7 Crie uma classe chamada Pedido com:
+
+//Propriedade pública NumeroPedido (string).
+//Propriedade pública NomeCliente (string).
+//Propriedade pública Status (string).
+//Um método chamado AtualizarStatus(string novoStatus) para alterar o status.
+//Um método chamado ExibirPedido() que mostre os dados na tela.
+
+Pedido pedido = new("1","Guilherme","EM PROCESSAMENTO");
+Console.WriteLine("{0}",pedido.ExibirPedido);
+pedido.AtualizarStatus("A CAMINHO");
+Console.WriteLine("{0}", pedido.ExibirPedido);
+
+
+//8 Crie uma classe chamada Consulta com:
+
+//Propriedade pública NomePaciente.
+//Propriedade pública NomeMedico.
+//Propriedade pública DataConsulta (do tipo DateTime).
+//Método Reagendar(DateTime novaData) para atualizar a data.
+//Método ExibirResumo() que mostra as informações da consulta, usando "Data:" na primeira exibição e "Nova data:" se ela tiver sido reagendada.
+
+Consulta consulta = new("Guilherme","Roger");
+consulta.AtualizarData(new DateTime(2026,02,18,20,0,0));
+consulta.AtualizarData(DateTime.Parse("18/01/2026 20:00",CultureInfo.GetCultureInfo("pt-BR")));
+consulta.ExibirResumo();
+
+
+//9 Crie:
+
+//Uma classe InformacaoTecnica com:
+//Propriedades TamanhoMB e SistemaOperacional.
+//Uma classe ProdutoDigital com:
+//Propriedades Nome, Preco e InfoTecnica (do tipo InformacaoTecnica).
+//Um método ExibirDetalhes() para mostrar tudo na tela.
+
+InformacaoTecnica info = new();
+info.SistemaOperacional = "Windows";
+info.TamanhoMB = 512;
+ProdutoDigital produtoDigital = new(info)
+{
+    Nome = "Memoria",
+    Preco = 250,
+};
+
+Console.WriteLine(produtoDigital.ExibirDetalhes);
+
+public class InformacaoTecnica
+{
+    public int TamanhoMB { get; set; }
+    public string SistemaOperacional { get; set; }
+}
+
+public class ProdutoDigital
+{
+    public ProdutoDigital(InformacaoTecnica informacao)
+    {
+        InfoTecnica = informacao;
+    }
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
+    public InformacaoTecnica InfoTecnica { get; set; }
+
+    public string ExibirDetalhes => $"Nome {Nome} - Preco {Preco} - TamanhoMB {InfoTecnica.TamanhoMB} - SISTEMA OPERACIONAL {InfoTecnica.SistemaOperacional}";
+}
+
+
+
+public class Consulta
+{
+    public Consulta(string nomePaciente, string nomeMedico)
+    {
+        this.NomePaciente = nomePaciente;
+        this.NomeMedico = nomeMedico;
+    }
+
+    public string NomePaciente { get; set; }
+    public string NomeMedico { get; set; }
+    public DateTime DataConsulta { get; set; }
+
+    public DateTime AtualizarData(DateTime date)
+    {
+        return DataConsulta = date;
+    }
+    public void ExibirResumo()
+    {
+        Console.WriteLine($"{NomePaciente},{NomeMedico},{DataConsulta}");
+    }
+}
+
+public class Pedido
+{
+    public Pedido(string num, string nome, string status)
+    {
+        this.NumeroPedido = num;
+        this.NomeCliente = nome;
+        this.Status = status;
+    }
+
+    public string NumeroPedido { get; set; }
+    public string NomeCliente { get; set; }
+    public string Status { get; set; }
+    public string ExibirPedido => $"NOME {NomeCliente} - NUMERO {NumeroPedido} - STATUS {Status}";
+
+    public void AtualizarStatus(string novoStatus)
+    {
+        if (string.IsNullOrWhiteSpace(novoStatus))
+            throw new Exception("NOVO STATUS INVALIDO");
+        this.Status = novoStatus;
+    }
+}
 
 public class Produto
 {
